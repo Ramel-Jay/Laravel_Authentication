24590a0d95fdddef4ed001e440d5a1eae7a97f97
Ramel Jay <rameljey15@gmail.com>
2023-05-20 12:02:10 +0800

    change the users to players and add migrations

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,7 +21,7 @@
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('users')" :active="request()->routeIs('users')">
-                        {{ __('Users') }}
+                        {{ __('Players') }}
                     </x-nav-link>
                 </div>
             </div>
